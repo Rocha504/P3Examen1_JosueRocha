6247b340dde181a7140b7226d7ac8e292f8d1a04
Rocha504 <josuerocha504@gmail.com>
2021-08-20 19:21:11 -0400

    Continuando el ping

--- Red.cpp
+++ Red.cpp
@@ -1,6 +1,8 @@
 #include "Red.h"
 #include <iostream>
 using namespace std;
+#include <vector>
+#include <sstream> 
 
 Red::Red()
 {
@@ -36,23 +38,32 @@ void Red :: ingresar(int select){
 		}	
 		else
 			if(comando.find("ping") != string::npos){
-		    	
+				int n1,n2,n3,n4;
 				string ip1=	this->pcs[select]->getIP();
 				string mask1=this->pcs[select]->getMaskred();
 				string ip2=comando.substr(5);
 				string mask2;
 				for(int i=0;i<this->pcs.size();i++){
 					if(this->pcs[i]->getIP()==ip2){
-						mask2=this->pcs[i]->getIP();
+						mask2=this->pcs[i]->getMaskred();
 					}
 				}
+				vector <string> numeros;
+				string split;
+				stringstream prueba(ip1);
+				while(getline(prueba,split,'.')){
+       				numeros.push_back(split);
+    			}
+    		    n1=stoi(numeros[0]);
+    		    n2=stoi(numeros[1]);
+    		    n3=stoi(numeros[2]);
+    		    n4=stoi(numeros[3]);
+        		 
 				
 				
-					 
 			}
-		}
+	}
 }
-
 Red::~Red()
 {
 	//recorrer vector y eliminar
